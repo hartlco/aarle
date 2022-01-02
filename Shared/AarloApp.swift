6d0076086677a558f6f9b51b487cf55057e20927
Martin Hartl <martin@hartl.co>
2022-01-02 11:43:54 +0100

    Initial Commit

---
+++ Shared/AarloApp.swift
@@ -0,0 +1,17 @@
+//
+//  AarloApp.swift
+//  Shared
+//
+//  Created by martinhartl on 02.01.22.
+//
+
+import SwiftUI
+
+@main
+struct AarloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
